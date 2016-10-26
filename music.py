b66b478bd82458a568e4b3b2026784455129cd7a
katgni <40212143@live.napier.ac.uk>
2016-10-26 03:01:31 +0100

    Add files via upload
    final version

--- music.py
+++ music.py
@@ -5,6 +5,8 @@ import utils
 class Music(flask.views.MethodView):
     @utils.login_required
     def get(self):
-        songs = os.listdir('static/music')
-        return flask.render_template("music.html", songs=songs)
+        
+		
+		
+        return flask.render_template("music.html",)
     
